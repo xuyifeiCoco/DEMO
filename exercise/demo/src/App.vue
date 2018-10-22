c4ce378bd0ecdd7d7a3f1a562ff928a58a942dc8
xuyifei <xuyifei@lanjingren.com>
2018-10-22 09:42:27 +0800

    添加日常练习

---
+++ exercise/demo/src/App.vue
@@ -0,0 +1,59 @@
+<template>
+  <div id="app">
+    <ul>
+      <template v-for='(item , index) in routerArray'>
+        <router-link tag="li" :to="item.path" v-bind:class="{ 'red': $route.path===item.path}" :key="index">
+          {{item.value}}
+        </router-link>
+      </template>
+    </ul>
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data(){
+    return {
+      routerArray:[
+        {
+          path:'/Axios',
+          value:'请求'
+        },
+        {
+          path:'/',
+          value:"首页"
+        }
+      ]
+    }
+  },
+  mounted(){
+    // console.log(this.$route)
+  }
+};
+</script>
+
+<style scoped>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+ul{
+  width: 100%;
+  overflow: hidden
+}
+ul li{
+  float: left;
+  cursor: pointer;
+  list-style: none;
+  margin-right: 30px;
+}
+.red{
+  color: red;
+}
+</style>
---
+++ exercise/demo/src/components/Axios.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    axios
+  </div>
+</template>
+<script>
+import axios from "axios";
+export default {
+  name: "Axios",
+  mounted() {
+    //==========测试formdata请求
+    axios
+      .post("http://t-www.meipian.cn/promo/checkin_duiba/api/autoLogin", {
+        user_id: "30401012",
+        avatar:
+          "http://t-static2.ivwen.com/users/30401012/2363631c1ac547abacdd230cb62d390f.jpg?meipian/bucket/ivwen/key/dXNlcnMvMzA0MDEwMTIvMjM2MzYzMWMxYWM1NDdhYmFjZGQyMzBjYjYyZDM5MGYuanBn/sign/8b98284a5f60c3cd284c7b5640378b11",
+        nickname: "Yajun",
+        token: "e5b4591ec3109712ea94b1685679409c",
+        weibo_bind: 0,
+        phone_bind: 0
+      })
+      .then(function(response) {
+        console.log(response);
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+  }
+};
+</script>
