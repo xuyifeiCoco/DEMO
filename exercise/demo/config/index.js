1d83c1ec46ac90491cb6c17dde6b2e9bb2ac6c41
LAPTOP-OMQM6I2G\hp <1227162950@qq.com>
2018-10-28 16:06:33 +0800

    添加chrome和webpack跨域

--- exercise/demo/config/index.js
+++ exercise/demo/config/index.js
@@ -10,7 +10,13 @@ module.exports = {
     // Paths
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
-    proxyTable: {},
+    proxyTable: {
+      // '/user/*':{
+      //   target: 'http://192.168.67.57:8081',
+      //   changeOrigin: true,
+      //   secure: false
+      // }
+    },
 
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
--- exercise/demo/src/components/Axios.vue
+++ exercise/demo/src/components/Axios.vue
@@ -8,6 +8,11 @@ import axios from "axios";
 import Qs from "qs";
 export default {
   name: "Axios",
+  data(){
+    return {
+      domain:'http://192.168.67.57:8081'
+    }
+  },
   methods: {
     formAxios() {
       //==========测试formdata请求  参数为数组
@@ -26,11 +31,35 @@ export default {
         .then(res => {
           console.log(res.data);
         });
+    },
+
+    //测试跨域请求
+    /*
+    一、设置webpack
+      '/user/*':{
+        target: 'http://192.168.67.57:8081',
+        changeOrigin: true,
+        secure: false
+      }
+    二、设置chrome
+        mac  open -n /Applications/Google\ Chrome.app/ --args --disable-web-security --user-data-dir=/Users/xuyifei/Documents/network/chrome
+        window  "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --disable-web-security --user-data-dir=C:\chromeCross  在右键属性里面设置
+    */ 
+    crossAxios(){  //http://192.168.67.57:8081/user
+        axios.get(`/user`).then((res) =>{
+          console.log(res)
+        })
+    },
+    chromeCross(){
+      axios.get(`${this.domain}/user`).then((res) =>{
+          console.log(res)
+        })
     }
   },
   mounted() {
     // this.formAxios()
-    
+    // this.crossAxios()
+    // this.chromeCross()
   }
 };
 </script>
